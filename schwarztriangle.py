106b2f7c1b954de1caaa96255e662c5087312065
Ine Gabrielsen <ine.gabrielsen@student.uib.no>
2021-06-23 15:47:44 +0200

    Make a class for Schwarz triangle tessellations.

---
+++ schwarztriangle.py
@@ -0,0 +1,19 @@
+import math
+
+class SchwarzTriangle:
+    """This class implements a tessellation of the hyperbolic plane by Schwarz triangles.
+    Feel free to remove this if it is unnecessary, or to change names of things. """
+
+    def __init__(self, p, q, r):
+        assert p > 2 and q > 2 and r > 2
+        self.angle1 = math.pi/p
+        self.angle2 = math.pi/q
+        self.angle3 = math.pi/r
+
+    def make_triangle():
+        """Makes a triangle with the given angles."""
+        #TODO
+
+    def make_tessellation():
+        """Makes a tessellation by reflecting the triangle."""
+        #TODO

614ddec1eac3f7c709687e55704da0b36fa1f048
Mara Popescu <mara.eliana.popescu@gmail.com>
2021-06-23 08:33:34 +0300

    comment update

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import tkinter
 from h2geometry import H2_segment, H2_reflection
 
 def test_H2_reflection(window):
-    #Strange behaviour when z3 is on the geodesic between z1 and z2
-    z1=0.5-0.4j
-    z2=-0.9-0.1j
+    z1=0.5+0.5j
+    z2=-0.3-0.3j
     window.canvas.draw_H2_segment(z1, z2, "cyan")
     s=H2_segment(z1, z2)
-    z3=0.2+0.2j
+    z3=0.2+0.3j
     s_ref=H2_reflection(s)
     z3_ref=s_ref.reflect(z3)
+    print("z3_ref={}".format(z3_ref))
     window.canvas.draw_H2_segment(z3, z3_ref, "green", complete=False)
 
 def reflect_triangle(window, s, z1, z2, z3):
@@ -24,12 +24,12 @@ def reflect_triangle(window, s, z1, z2, z3):
     window.canvas.draw_H2_triangle(z1_ref, z2_ref, z3_ref, "purple")
 
 def test_reflect_triangle(window):
-    z1=0.2+0.2j
+    z1=0.2+0.7j
     z2=-0.5-0.2j
     s=H2_segment(z1, z2)
     window.canvas.draw_H2_segment(z1, z2)
     s_ref=H2_reflection(s)
-    z11 = -0.3 + 0.5j
+    z11 = 0.1 + 0.9j
     z12 = -0.5 + 0.1j 
     z13 = -0.9 + 0.1j
     reflect_triangle(window, s_ref, z11, z12, z13)
@@ -57,9 +57,9 @@ if __name__ == "__main__":
 
     #Test for reflecting a triangle
     #------------------------uncomment the next line for testing
-    #test_reflect_triangle(main_window)
+    test_reflect_triangle(main_window)
     
-    # Test for draw_H2_triangle
+    '''# Test for draw_H2_triangle
     z11 = 0.5 + 0.5j
     z12 = -0.5 + 0.5j 
     z13 = -0.2 - 0.7j
@@ -71,5 +71,5 @@ if __name__ == "__main__":
     z23 = -0.2 - 0.2j
     z24 = 0.2 - 0.2j
     z = [z21,z22,z23,z24]
-    main_window.canvas.draw_H2_polygon(z, "magenta")
+    main_window.canvas.draw_H2_polygon(z, "magenta")'''
     main_window.run()

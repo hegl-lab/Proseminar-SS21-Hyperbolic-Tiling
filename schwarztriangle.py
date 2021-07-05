8c586e5f0879c24454094215a120d80a9d676723
Ine Gabrielsen <ine.gabrielsen@student.uib.no>
2021-07-05 13:46:10 +0200

    Change name of make_triangle to get_vertices

--- schwarztriangle.py
+++ schwarztriangle.py
@@ -8,8 +8,9 @@ class SchwarzTriangle:
         self.angle1 = math.pi/p
         self.angle2 = math.pi/q
         self.angle3 = math.pi/r
+        self.vertices = self.get_vertices()
 
-    def make_triangle():
-        """Makes a triangle with the given angles."""
+    def get_vertices(self):
+        """Returns a list of the vertices of a triangle with the given angles."""
 
         
